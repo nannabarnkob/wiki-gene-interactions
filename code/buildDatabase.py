c83b023e311145effffe7a9ea901254bba57f49a
nannabarnkob <nannambarnkob@gmail.com>
2018-11-08 15:21:05 +0100

    Template done

--- code/buildDatabase.py
+++ code/buildDatabase.py
@@ -1,9 +1,21 @@
 #! /usr/bin/env python
 
 from BloomFunctions import BloomFunctions
+import sqlite3
 
-bloomfilter = BloomFunctions('../data/gene_symbol_list.txt')
 
-for line in XML:
-    if bloomfilter.classify(word):
-        "INSERT word INTO interactiontable.partners WHERE gene == y"
+class BuildDataBase:
+    def connect_database(databaseName):
+        self.db = sqlite3.connect(databaseName)
+        self.cursor = self.db.cursor()
+
+    def close(self):
+        self.db.close()
+
+    def main():
+        self.bloomfilter = BloomFunctions('../data/gene_symbol_list.txt')
+
+    def process_wiki():
+        for line in XML:
+            if self.bloomfilter.classify(word):
+                "INSERT word INTO interactiontable.partners WHERE gene == y"
